a1211e798734ce6d7fc8464dfb2355ede1d67935
jipsterk <jipsterk1998@gmail.com>
2019-05-31 00:28:29 +0200

    fix no dowloads

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 import "dotenv/config";
+import fs from "fs";
 import cron from "node-cron";
 import path from "path";
 import puppeteer from "puppeteer";
+import util from "util";
 
 cron.schedule("0 0 * * 1", async () => {
-  await getHtml();
+  await getICS();
+  // await importIntoCalender();
 });
 
-async function getHtml() {
-  const browser = await puppeteer.launch({ headless: true });
+/**
+ * Gets .ics file
+ * @returns {Promise<void>}
+ */
+async function getICS() {
+  const browser = await puppeteer.launch({
+    headless: process.env.NODE_ENV === "production"
+  });
   const page = await browser.newPage();
   await page.goto("https://sa-nhlstenden.xedule.nl");
 
-  await page.type("#userNameInput", process.env.USERNAME);
-  await page.type("#passwordInput", process.env.PASSWORD);
-  await page.click("#submitButton");
-  await page.waitForSelector(".search-toggle");
+  try {
+    await manualLogin(page);
+  } catch (error) {
+    await page.authenticate({
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD
+    });
+  }
 
+  await page.waitForSelector(".search-toggle");
   await page.click(".search-toggle");
 
   for (const text of [
@@ -32,12 +46,9 @@ async function getHtml() {
   await page.click(".search-toggle");
   await page.click("li.k-current-view>a.k-link");
 
-  const downloadPath = path.resolve(
-    process.cwd(),
-    `download-${Math.random()
-      .toString(36)
-      .substr(2, 8)}`
-  );
+  const downloadPath = path.resolve(__dirname, "downloads");
+  if (!util.promisify(fs.exists)(downloadPath))
+    await util.promisify(fs.mkdir)(downloadPath);
 
   await page._client.send("Page.setDownloadBehavior", {
     behavior: "allow",
@@ -45,7 +56,28 @@ async function getHtml() {
   });
 
   await page.click(".k-view-ics");
+
+  let fileName;
+  while (!fileName || fileName.endsWith(".crdownload")) {
+    await new Promise(resolve => setTimeout(resolve, 100));
+    [fileName] = await util.promisify(fs.readdir)(downloadPath);
+  }
   await browser.close();
 }
 
-getHtml();
+/**
+ * fallback for loggin in
+ * @param {puppeteer.Page} page
+ * @returns {Promise<void>}
+ */
+async function manualLogin(page) {
+  try {
+    await page.type("#userNameInput", process.env.USERNAME);
+  } catch (error) {
+    throw new Error();
+  }
+  await page.type("#passwordInput", process.env.PASSWORD);
+  await page.click("#submitButton");
+}
+
+getICS();
